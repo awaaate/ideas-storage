89628b1c8804a138c051c0efb54fb7d3c57c4206
awamas <tawatetb04@gmail.com>
2019-12-25 21:44:32 +0100

    Updates

---
+++ precache-manifest.3642868fd93216d5b582e4a820a0bb4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9737a30b99516a02aa562fab3e9340eb",
+    "url": "/ideas-storage/index.html"
+  },
+  {
+    "revision": "b987cb41839038f58141",
+    "url": "/ideas-storage/static/css/main.ba5cce99.chunk.css"
+  },
+  {
+    "revision": "fd7c61b6465a5e93a65a",
+    "url": "/ideas-storage/static/js/2.bf61b328.chunk.js"
+  },
+  {
+    "revision": "eaa93899759cd78ff2a4147c0af30aa2",
+    "url": "/ideas-storage/static/js/2.bf61b328.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b987cb41839038f58141",
+    "url": "/ideas-storage/static/js/main.96111de8.chunk.js"
+  },
+  {
+    "revision": "0d1a4ffd76d993084bd5",
+    "url": "/ideas-storage/static/js/runtime-main.9ca79d44.js"
+  },
+  {
+    "revision": "ca966a1c5c2202eae8a1389198b1edfe",
+    "url": "/ideas-storage/static/media/Icon ionic-md-open.ca966a1c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ideas-storage/static/media/logo.5d5d9eef.svg"
+  }
+]);
